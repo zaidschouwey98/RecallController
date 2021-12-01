7c695b8946c73ecba4cc4e5ee32db84856ed4e92
zaidschouwey98 <zazar981@hotmail.com>
2021-12-01 11:27:55 +0100

    Add pause and increase and decrease recoil

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ frm = ttk.Frame(root, padding=10)
 frm.grid()
 ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+recoil_x_value = 5
+recoil_y_value = 0
 
-
-def control():
-    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(0), int(4), 0, 0)
-
+def interrupt():
+    ctrl_state = win32api.GetKeyState(0x11)
+    tkey_state = win32api.GetKeyState(0x54)
+    if(ctrl_state < -1 and tkey_state < -1):
+        return True
+    else:
+        return False
 
 def is_mouse_down():    # Returns true if the left mouse button is pressed
     lmb_state = win32api.GetKeyState(0x01)
@@ -21,33 +26,43 @@ def is_mouse_down():    # Returns true if the left mouse button is pressed
         return True
     else:
         return False
+def increase_recoil():
+    global recoil_x_value
+    recoil_x_value-=1
+
+def lower_recoil():
+    global recoil_x_value
+    recoil_x_value+=1
+def modify_recoil():
+    numpad_10 = win32api.GetKeyState(0x68)
+    if(numpad_10 < -1):
+        increase_recoil()
+
+    numpad_2 = win32api.GetKeyState(0x62)
+    if(numpad_2 < -1):
+        global recoil_x_value
+        lower_recoil()
+
+    
 
 
-
-while(1):
+while(interrupt() == False):
+    modify_recoil()
     if(is_mouse_down()):
-        for x in range(0, 55): # 55
+        for x in range(0, 55):
             # start = time.time()
             time.sleep(0.016)
             if(is_mouse_down()):
                 
-                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(0), int(9), 0, 0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(recoil_y_value), int(recoil_x_value), 0, 0)
                 # print(f'Time: {time.time() - start}')
             else:
                 break
         for x in range(0, 20):
-            time.sleep(0.015)
+            time.sleep(0.016)
             if(is_mouse_down()):
-                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(0), int(9), 0, 0)
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(recoil_y_value), int(recoil_x_value), 0, 0)
             else:
                 break
-# mouse.on_button(control ,args=(), buttons=('left'), types=('down'))
-    # mouse.wait(button='right', target_types=( 'down'))
-    # while(mouse.is_pressed(button='right') and mouse.is_pressed(button="left")):
-    #     win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(0), int(1), 0, 0)
-        # print("test")
-        # pos = mouse.get_position()
-        # mouse.move(pos[0], pos[1]+4, absolute=True, duration=0)
-
     time.sleep(0.002)
 
